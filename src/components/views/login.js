297e2826bd38723506fcc10dd638931e01396ca4
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-02-16 11:42:00 -0700

    added login.js

---
+++ src/components/views/login.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './main.css';
+
+class Login extends React.Component{
+
+    render() {
+        return (
+        <div className="content">
+            <h1>Please login to see emails</h1>
+            <div className="main-content-border">
+                
+            </div>
+            
+       
+        </div>
+        )
+    }
+
+
+
+
+};
+
+export default Login;
